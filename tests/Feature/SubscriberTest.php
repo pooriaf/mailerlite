3e50e66af9778b71de283b0b4cbe387ef0d514ab
Pooria <pooriaf@gmail.com>
2018-11-24 21:57:47 +0330

    add tests for subscribers

--- tests/Feature/SubscriberTest.php
+++ tests/Feature/SubscriberTest.php
@@ -42,4 +42,44 @@ class SubscriberTest extends TestCase
             'data' => true
         ]);
     }
+
+    public function testCanSubscribeWithCustomFields()
+    {
+        $subscriber = factory(Subscriber::class)->state('new_subscriber')->make();
+        $fields = factory(Field::class, 3)->create();
+        foreach ($fields as $field) {
+            $subscriber->{$field->title} = FieldFakerHelper::getTitle($this->faker, $field->type);
+        }
+        $response = $this->json('POST', route('subscribers.store'), $subscriber->toArray());
+        $response->assertStatus(201);
+        $response->assertJson([
+            'data' => true
+        ]);
+    }
+
+    public function testCanUpdateSubscriberWithCustomFields()
+    {
+        $subscriber = factory(Subscriber::class)->state('new_subscriber')->create();
+
+        $payload = [
+            'state' => $this->faker->randomElement(Subscriber::STATE)
+        ];
+        $fields = factory(Field::class, 2)->create();
+        foreach ($fields as $field) {
+            $payload[$field->title] = FieldFakerHelper::getTitle($this->faker, $field->type);
+        }
+
+        $response = $this->json('PATCH', route('subscribers.update', $subscriber->id), $payload);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => true
+        ]);
+    }
+
+    public function testCanRemoveSubscriber()
+    {
+        $subscriber = factory(Subscriber::class)->state('random_subscriber')->create();
+        $response = $this->json('DELETE', route('subscribers.destroy', $subscriber->id), $subscriber->toArray());
+        $response->assertStatus(200);
+    }
 }
